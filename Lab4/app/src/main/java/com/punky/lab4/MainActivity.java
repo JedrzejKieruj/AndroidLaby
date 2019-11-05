9aa65431f8ca3248e84b2fae159e22c5fd957024
JK <jedrzej.kieruj@gmail.com>
2019-11-05 13:27:37 +0100

    laby laby

---
+++ Lab4/app/src/main/java/com/punky/lab4/MainActivity.java
@@ -0,0 +1,20 @@
+package com.punky.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addClick(View view) {
+        EditText taskTitleEditTxt
+    }
+}
